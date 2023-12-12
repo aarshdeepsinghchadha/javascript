d8be08ee27f1ec9dce86a3b1ecddb0999dc6855b
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2023-12-12 22:48:43 +0530

    variables concept

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "asc@test.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+let accountState;
+
+// accountId = 2
+
+
+
+accountEmail = "hc@gmail.com"
+accountPassword = "21324"
+accountCity = "baroda"
+
+
+/*
+
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword, accountCity, accountState]);
