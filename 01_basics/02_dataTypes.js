f0bdb05c82b2b953c7bd4691660d1c3b9e3e4574
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2023-12-14 08:31:19 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all JS Code as new version
+
+
+//alert( 3 + 3) // we are using node js, not browser
+
+console.log(3 + 3)
+
+console.log("aarsh")
+
+let name = "aarsh"
+let age = 23
+let isLoggedIn = true
+
